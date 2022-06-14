89afc09a324913cab1b913dbb523c1a3498f0a6b
jarekTerejko <j.terejko@gmail.com>
2022-06-14 08:35:09 +0200

    create DescriptionTextEl

---
+++ src/components/DescriptionText/DescriptionTextEl.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const DescriptionTextEl = styled.p`
+  color: var(--color-light-grey);
+  letter-spacing: -0.3px;
+  line-height: 2.4rem;
+  max-width: 37rem;
+  margin-bottom: 4.2rem;
+  opacity: 0.8;
+`;
