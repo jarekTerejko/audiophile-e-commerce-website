79c78771e4337424a29354645c297071a080bcc8
jarekTerejko <j.terejko@gmail.com>
2022-06-14 08:36:26 +0200

    create LinkRouter

---
+++ src/components/LinkRouter/LinkRouterEl.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const LinkRouter = styled(Link)`
+  background: var(--color-orange);
+  color: var(--color-white);
+  padding: 1.55rem 3.1rem;
+  text-transform: uppercase;
+  font-size: 1.2rem;
+  letter-spacing: 1.5px;
+  font-weight: 700;
+`;
