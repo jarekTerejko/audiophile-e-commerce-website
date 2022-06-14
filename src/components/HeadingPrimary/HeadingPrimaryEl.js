dcc100796020e2b5e0dc3b6e3458448bb873ac24
jarekTerejko <j.terejko@gmail.com>
2022-06-14 08:35:48 +0200

    create HeadingPrimaryEl

---
+++ src/components/HeadingPrimary/HeadingPrimaryEl.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HeadingPrimaryEl = styled.h1`
+  color: var(--color-white);
+  font-size: 56px;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  line-height: 5.8rem;
+  margin-bottom: 2.5rem;
+`;
