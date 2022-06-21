7d0c936732c81379581743673cd54c3bee6789fa
jarekTerejko <j.terejko@gmail.com>
2022-06-21 08:24:51 +0200

    create HeadindSecondaryEl

---
+++ src/components/HeadingSecondary/HeadingSecondaryEl.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const HeadingSecondaryEl = styled.h2`
+  font-size: 28px;
+  font-weight: 700;
+  letter-spacing: 2px;
+  
+  &.featured {
+    position: absolute;
+    text-transform: uppercase;
+    top: 100px;
+    left: 95px;
+  }
+`;
