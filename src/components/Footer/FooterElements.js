d7d190ce7a935027fe848ce1ce7d4a8d87483d8a
jarekTerejko <j.terejko@gmail.com>
2022-06-30 06:58:59 +0200

    add tablet styles to Footer

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -4,11 +4,21 @@ export const FooterWrapper = styled.footer`
   min-height: 36.5rem;
   background: #101010;
   padding-top: 7.5rem;
+
+  @media screen and (max-width: 768px) {
+    padding-top: 6rem;
+    padding-bottom: 41px;
+  }
 `;
+
 export const FooterContent = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   position: relative;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+  }
 
   &::before {
     content: "";
@@ -18,22 +28,48 @@ export const FooterContent = styled.div`
     height: 4px;
     width: 10rem;
     background: var(--color-orange);
+
+    @media screen and (max-width: 768px) {
+      top: -60px;
+    }
   }
 `;
-export const FooterContentRowOneColLeft = styled.div``;
+
+export const FooterContentRowOneColLeft = styled.div`
+  @media screen and (max-width: 768px) {
+    margin-bottom: 27px;
+  }
+`;
+
 export const FooterContentRowOneColRight = styled.nav``;
+
 export const FooterContentRowTwo = styled.div`
   grid-column: 1/3;
   padding-top: 3.1rem;
 `;
+
 export const FooterContentRowThreeColLeft = styled.div`
   padding-top: 1.4rem;
+
+  @media screen and (max-width: 768px) {
+    padding-top: 39px;
+  }
 `;
+
 export const FooterContentRowThreeColRight = styled.div``;
 export const FooterNavItems = styled.ul`
   display: flex;
   justify-content: flex-end;
   list-style: none;
+
+  @media screen and (max-width: 768px) {
+    justify-content: flex-start;
+    margin-left: -5px;
+  }
+
+  @media screen and (max-width: 580px) {
+    flex-direction: column;
+  }
 `;
 
 export const FooterNavItem = styled.li`
@@ -51,9 +87,24 @@ export const FooterSocialItems = styled.ul`
   align-items: center;
   list-style: none;
   margin-top: -7.3rem;
+
+  @media screen and (max-width: 768px) {
+    margin-top: -65px;
+  }
+
+  @media screen and (max-width: 580px) {
+    justify-content: flex-start;
+    margin-top: 0;
+  }
 `;
+
 export const FooterSocialItem = styled.li`
   margin-left: 16px;
+
+  @media screen and (max-width: 580px) {
+    margin-left: 0;
+    margin-right: 16px;
+  }
 `;
 
 export const FooterSocialLink = styled.a``;
